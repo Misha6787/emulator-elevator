2bae97d06f705c708c2d8947cf1807567b53f2d7
misha <misafilimon255@mail.ru>
2024-05-06 17:18:21 +0300

    Добавление сохранения текущего состояний лифта в LocalStorage, небольшой рефакторинг и финальные правки основной версии

--- src/components/Elevator/Elevator.vue
+++ src/components/Elevator/Elevator.vue
@@ -29,7 +29,7 @@
 <script setup>
     import './Elevator.scss'
 
-    import {computed, ref} from "vue";
+    import {computed, onMounted, ref, watch} from "vue";
 
     import Floor from "./Floor/Floor.vue";
     import ElevatorCabin from "./ElevatorCabin/ElevatorCabin.vue";
@@ -78,17 +78,59 @@
         return beforeFloor.value - activeFloor.value > 0 ? beforeFloor.value - activeFloor.value : activeFloor.value - beforeFloor.value
     })
 
-    // Вызов лифта на этаж
+    // Переключаем статус индикатора кабины лифта на время "отдыха" лифта
+    const activationIndicator = () => {
+        elevatorCabinIndicatorStatus.value = false
+
+        setTimeout(() => {
+            elevatorCabinIndicatorStatus.value = true
+        }, props.delayTime)
+    }
+
+    // Сохранение значений в localStorage
+    const setDataInLocalStorage = () => {
+        localStorage.setItem('activeFloor', JSON.stringify(activeFloor.value))
+        localStorage.setItem('elevatorCabinCallStack', JSON.stringify(elevatorCabinCallStack.value))
+    }
+
+    // Получение данных из LocalStorage и запуск перехода этажей на их основе
+    const getDataInLocalStorage = () => {
+        // Проверяем, есть ли данные в LocalStorage
+        if (!localStorage.getItem('activeFloor') || !localStorage.getItem('elevatorCabinCallStack')) return
+        activeFloor.value = JSON.parse(localStorage.getItem('activeFloor'))
+        elevatorCabinCallStack.value = JSON.parse(localStorage.getItem('elevatorCabinCallStack'))
+
+        // Проверяем, совпадает ли текущий этаж с первым значением в массиве стека вызовов, если да, то удаляем его
+        if (activeFloor.value === elevatorCabinCallStack.value[0]) elevatorCabinCallStack.value.shift()
+
+        // Проверяем, есть ли данные в стеке вызовов
+        if (elevatorCabinCallStack.value.length > 0) {
+            setTimeout(() => {
+                changeFloor(elevatorCabinCallStack.value[0], true)
+                elevatorCabinCallStack.value.shift()
+            }, props.delayTime)
+
+            // Активируем индикатор кабины лифта
+            activationIndicator()
+        }
+    }
+
+    /**
+     * Вызов лифта на этаж
+     *
+     * @param {number} floor — этаж на который должен прибыть лифт
+     * @param {boolean} autoChange — параметр, обозначающий, что функция выполняется автоматически
+     * @returns {void}
+     *
+     */
     const changeFloor = (floor, autoChange = false) => {
         // Если мы пытаемся вызвать лифт на этаж, на котором он уже находится
         if (floor === activeFloor.value) return
 
         // Если кабина не готова к переходу, то добавляем этаж в стек вызовов
-        // autoChange нужен для разграничевания автоматической смены этажей и ручной
+        // autoChange нужен для разграничения автоматической смены этажей и ручной
         if (!elevatorCabinStatus.value && !autoChange) {
-            if (!elevatorCabinCallStack.value.find(item => floor === item)) {
-                elevatorCabinCallStack.value.push(floor)
-            }
+            if (!elevatorCabinCallStack.value.find(item => floor === item)) elevatorCabinCallStack.value.push(floor)
             return
         }
 
@@ -99,7 +141,9 @@
 
         // Переключаем статус кабины лифта с учетом задержки
         const changeStatusCabinAndCallStack = () => {
+            // Переключаем статус кабины лифта
             elevatorCabinStatus.value = false
+
             setTimeout(() => {
                 if (elevatorCabinCallStack.value.length > 0) {
                     // Запускаем следующий этаж
@@ -113,13 +157,10 @@
             }, elevatorCabinSpeed.value * 1000 + props.delayTime)
         }
 
-        // Переключаем статус индикатора кабины лифта на время "отдыха" лифта
+        // Переключаем статус индикатора после задержки перехода лифта
         const changeElevatorCabinIndicatorStatus = () => {
             setTimeout(() => {
-                elevatorCabinIndicatorStatus.value = false
-                setTimeout(() => {
-                    elevatorCabinIndicatorStatus.value = true
-                }, props.delayTime)
+                activationIndicator()
             }, elevatorCabinSpeed.value * 1000)
         }
 
@@ -135,4 +176,12 @@
         changeElevatorCabinIndicatorStatus()
         changeElevatorCabinStatusFloor()
     }
+
+    onMounted(() => {
+        getDataInLocalStorage()
+    })
+
+    watch([() => activeFloor.value, () => elevatorCabinCallStack.value], () => {
+        setDataInLocalStorage()
+    }, {deep: true})
 </script>
